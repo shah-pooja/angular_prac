93862a148bdb1d912a57dfeb191bf00d31406adb
shah-pooja <pooja.shah1@solutionanalysts.com>
2017-10-23 10:18:35 +0530

    Add Pipes

---
+++ Pipes/demo/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  servers = [
+    {
+      instanceType : 'medium',
+      name : 'Production Server',
+      status : 'stable',
+      started : new Date(15, 1 ,2017)
+    },
+    {
+      instanceType : 'small',
+      name : 'User Server',
+      status : 'offline',
+      started : new Date(25, 2 ,2017)
+    },
+    {
+      instanceType : 'large',
+      name : 'Developement Server',
+      status : 'critical',
+      started : new Date(30, 3 ,2017)
+    }]
+
+    getStatusClasses(server: {instanceType : string, name : string, status : string, started : Date}){
+     return{
+       'list-group-item-success' : server.status === 'stable',
+       'list-group-item-warning' : server.status === 'offline',
+       'list-group-item-danger' : server.status === 'critical'
+     };
+    }
+}
